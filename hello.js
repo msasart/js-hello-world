6070caccff8963b5f6ae931cd45d22df7ec2f6c4
Shelby <shelby@Michelles-MacBook-Pro.local>
2023-08-19 14:03:44 -0500

    I updated the code LaunchCode provided.

--- hello.js
+++ hello.js
@@ -4,19 +4,20 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log(1+1)
-console.log(1+1)
-console.log(1+1)
-console.log(1+1)
-console.log('"Hello, World! \n This is Michelle."')
+const { Console } = require("console")
+
+console.log('"Hello, World!"')
+console.log(1+2+3+4)
+console.log("Hello, LaunchCode!\nThank you for helping me learn to code! :-)")
+
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed. Done by Michelle 
-   2. Figure out what the parentheses do. Will the code work without them? Gives me an unexpected string error.
+   1. Change the message that is printed. I did this.
+   2. Figure out what the parentheses do. Will the code work without them? I get an unexpected string error when removing them.
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   Removing one results in a Syntax error. Must use quote marks. Must use the same type of quote mark, but in the case using a single or double does not matter as long as they are the same.
+       Removing one results in a Syntax error. Must use quote marks. Must use the same type of quote mark, but in this case, using a single or double does not matter as long as they are the same.
    4. Remove the semi-colon, ;. Code still works in this case.
    5. Print a number. (Bonus: Print two numbers added together).Taking the quotes off allows for adding numbers.
    6. Print multiple messages one after the other. I repeated console.log on multiple lines.
@@ -24,3 +25,4 @@ Modify your code on line 7 to try to accomplish the tasks listed below:
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". I added single quote marks around double quote marks.
    9. Other. You choose!
 */
+
