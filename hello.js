c2c98a8ad955625e62a0adc1182d013e5765bf22
Shelby <shelby@Michelles-MacBook-Pro.local>
2023-08-19 12:05:13 -0500

    I updated the code as suggested.

--- hello.js
+++ hello.js
@@ -4,18 +4,23 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("2" + "2")
+console.log(1+1)
+console.log(1+1)
+console.log(1+1)
+console.log(1+1)
+console.log('"Hello, World! \n This is Michelle."')
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
+   1. Change the message that is printed. Done by Michelle 
+   2. Figure out what the parentheses do. Will the code work without them? Gives me an unexpected string error.
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   Removing one results in a Syntax error. Must use quote marks. Must use the same type of quote mark, but in the case using a single or double does not matter as long as they are the same.
+   4. Remove the semi-colon, ;. Code still works in this case.
+   5. Print a number. (Bonus: Print two numbers added together).Taking the quotes off allows for adding numbers.
+   6. Print multiple messages one after the other. I repeated console.log on multiple lines.
+   7. Print two messages on the same line. I think you do this by adding \n.
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". I added single quote marks around double quote marks.
    9. Other. You choose!
 */
